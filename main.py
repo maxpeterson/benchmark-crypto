1db7d705b93d8f6e2727fae6496d421818701c4b
Max Peterson <max@incuna.com>
2013-05-29 10:29:54 +0100

    Remove encryption vs decryption. Add BlowFish.

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
 import os
 import random
+import string
 
 import benchmark
-from Crypto.Cipher import AES
+from Crypto import Random
+from Crypto.Cipher import AES, Blowfish
 from SimpleAES import SimpleAES
 
 
-SECRET_KEY = os.urandom(16)
-IV = os.urandom(16)
+key = os.urandom(16)
 
-simpleAES = SimpleAES(SECRET_KEY)
-cryptoAES = AES.new(SECRET_KEY, AES.MODE_CBC, IV)
-BLOCK_SIZE = 16
-PADDING = '{'
+padchar = ' '
+
+simpleAES = SimpleAES(key)
 
 
-def _random_noise(len):
-    return ''.join(chr(random.randint(0, 0xFF)) for i in range(len))
+def _random_noise(N):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(N))
 
 
-def _pad_text(s, block_size, padding):
-    return s + (block_size - len(s) % block_size) * padding
+def _pad_text(plaintext, BS):
+    return plaintext + (BS - len(plaintext) % BS) * padchar
+
+
+def _unpad_text(plaintext):
+    return plaintext.rstrip(padchar)
+
+
+def encrypt_pycrypto_aes(plaintext):
+    bs = 16
+    iv = os.urandom(16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    padded = _pad_text(plaintext, bs)
+    return iv + cipher.encrypt(padded)
+
+
+def decrypt_pycrypto_aes(encrypted):
+    iv = encrypted[:16]
+    encrypted = encrypted[16:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted = cipher.decrypt(encrypted)
+    plaintext = _unpad_text(decrypted)
+    return plaintext
+
+
+def encrypt_pycrypto_bf(plaintext):
+    bs = Blowfish.block_size
+    iv = Random.new().read(bs)
+    cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+    padded = _pad_text(plaintext, bs)
+    return iv + cipher.encrypt(padded)
+
+
+def decrypt_pycrypto_bf(encrypted):
+    bs = Blowfish.block_size
+    iv = encrypted[:bs]
+    encrypted = encrypted[bs:]
+    cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+    decrypted = cipher.decrypt(encrypted)
+    plaintext = _unpad_text(decrypted)
+    return plaintext
 
 
 class Benchmark_Crypt(benchmark.Benchmark):
@@ -28,36 +67,40 @@ class Benchmark_Crypt(benchmark.Benchmark):
 
     def setUp(self):
         self.input_text = []
-        self.simple_aes_encrypted_text = []
-        self.crypto_aes_encrypted_text = []
-        for input_len in xrange(128):
+        for input_len in xrange(2, 128):
             for times in xrange(3):
                 plain_text = _random_noise(input_len)
                 self.input_text.append(plain_text)
-                self.simple_aes_encrypted_text.append(
-                    simpleAES.encrypt(plain_text)
-                )
-                self.crypto_aes_encrypted_text.append(
-                    cryptoAES.encrypt(
-                        _pad_text(plain_text, BLOCK_SIZE, PADDING)
-                    )
-                )
 
-    def test_simpleaes_encrypt(self):
+    def test_simpleaes(self):
         for text in self.input_text:
-            simpleAES.encrypt(text)
+            encrypted = simpleAES.encrypt(text)
+            decrypted = simpleAES.decrypt(encrypted)
+            assert text == decrypted
 
-    def test_simpleaes_decrypt(self):
-        for text in self.simple_aes_encrypted_text:
-            simpleAES.decrypt(text)
-
-    def test_pycrypto_encrypt(self):
+    def test_pycrypto_aes(self):
         for text in self.input_text:
-            cryptoAES.encrypt(_pad_text(text, BLOCK_SIZE, PADDING))
+            encrypted = encrypt_pycrypto_aes(text)
+            decrypted = decrypt_pycrypto_aes(encrypted)
+            assert text == decrypted
 
-    def test_pycrypto_decrypt(self):
-        for text in self.crypto_aes_encrypted_text:
-            cryptoAES.decrypt(text).rstrip(PADDING)
+    def test_pycrypto_blowfish(self):
+        for text in self.input_text:
+            encrypted = encrypt_pycrypto_bf(text)
+            decrypted = decrypt_pycrypto_bf(encrypted)
+            if text != decrypted:
+                print 'plaintext >{0}< ({1})'.format(text, len(text))
+                print 'decrypted >{0}< ({1})'.format(decrypted, len(decrypted))
+            assert text == decrypted
+
+
+    #def test_pycrypto_blowfish(self):
+    #    for text in self.input_text:
+    #        encrypted = cryptoBF.encrypt(_pad_text_aes(text, bs_bs))
+    #        decrypted = cryptoBF.decrypt(encrypted)
+    #        print '>{}<'.format(text)
+    #        print '>{}<'.format(decrypted)
+    #        #assert text == decrypted
 
 
 if __name__ == '__main__':
